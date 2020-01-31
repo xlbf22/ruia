e0bbd8616702c68138359f2e0c2610106f428e4e
mpauli <xlbf22@163.com>
2020-02-01 01:23:54 +0800

    update 2048

--- examples/simple_spider/test2048_spider.py
+++ examples/simple_spider/test2048_spider.py
@@ -11,23 +11,24 @@ str_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
 
 base_url = '''http://test2048.net/2048/'''
 base_path = '''/Users/mpauli/Desktop/caoliu'''
-ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+ua = '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'''
 
 @fl_middleware.request
 async def print_on_request(spider_ins, request):
+    # ua = 'ruia user-agent'
     request.headers.update({'User-Agent': ua})
 
 
 class CaoliuPageItem(Item):
-    target_item = TextField(xpath_select='//div[9]/table//tr[position()>9 and position()<55]/td[2]', default='')
+    target_item = TextField(xpath_select='//div[9]/table//tr[position()>10 and position()<55]/td[2]', default='')
     title = TextField(xpath_select='./a', default='')
     url = AttrField(xpath_select='./a', attr='href', default='')
 
 
 class CaoliuBodyItem(Item):
-    target_item = TextField(xpath_select='//*[@id="read_tpc"]/a', default='')
+    target_item = TextField(xpath_select='//*[@id="read_tpc"]/img', default='')
     title = TextField(xpath_select='//*[@id="subject_tpc"]', default='')
-    url = AttrField(xpath_select='./img', attr='src', default='')
+    url = AttrField(xpath_select='.', attr='src', default='')
     index = 0
 
 
@@ -36,17 +37,22 @@ class CaoliuSpider(Spider):
     concurrency = 3
 
     async def parse(self, response):
+        log.info("parse 0")
+        index = 0
         async for item in CaoliuPageItem.get_items(html=response.html):
-            yield Request(base_url+item.url, callback=self.parse_item)
+            index += 1
+            # if index > 1:
+            #     return
+            log.info("item.url={}".format(item.url))
+            yield Request(base_url+item.url, callback=self.parse_item, request_config={"RETRY_DELAY": 5})
 
     async def parse_item(self, response):
+        log.info("parse 1")
         index = 0
         async for item in CaoliuBodyItem.get_items(html=response.html):
             index += 1
             item.index = index
             yield item
-            if index > 3:
-                return
 
     async def process_item(self, item):
         # log.info("titem: {}".format(item))
